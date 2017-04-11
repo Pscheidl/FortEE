18be7abf4c1ea90dbb1ab3c8f75c8ed0c7f3a7ff
Pavel Pscheidl <pavel.junior@pscheidl.cz>
2017-04-11 20:05:44 +0200

    Execution Error event added

--- src/main/java/cz/pscheidl/benguard/FailsafeInterceptor.java
+++ src/main/java/cz/pscheidl/benguard/FailsafeInterceptor.java
@@ -1,13 +1,16 @@
 package cz.pscheidl.benguard;
 
+import cz.pscheidl.benguard.event.ExecutionError;
 import org.slf4j.Logger;
 
 import javax.annotation.Priority;
+import javax.enterprise.event.Event;
 import javax.inject.Inject;
 import javax.interceptor.AroundInvoke;
 import javax.interceptor.Interceptor;
 import javax.interceptor.InvocationContext;
 import java.io.Serializable;
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 /**
@@ -24,6 +27,9 @@ public class FailsafeInterceptor implements Serializable {
     @Inject
     private Logger logger;
 
+    @Inject
+    private Event<ExecutionError> executionErrorEvent;
+
     /**
      * If there is an exception thrown in the underlying method call, the exception is converted into an empty Optional.
      *
@@ -34,11 +40,25 @@ public class FailsafeInterceptor implements Serializable {
     public Object guard(InvocationContext invocationContext) {
         try {
             Object returnedObject = invocationContext.proceed();
-
             return returnedObject;
         } catch (Throwable throwable) {
+            throwExecutionErrorEvent(invocationContext, throwable);
             logger.warn("Failsafe interceptor exception caught.", throwable);
             return Optional.empty();
         }
+    }
+
+    /**
+     * Assembles and fires ExecutionError event.
+     *
+     * @param invocationContext Interceptor's invocation context
+     * @param throwable         Throwable catched by the interceptor
+     */
+    private void throwExecutionErrorEvent(InvocationContext invocationContext, Throwable throwable) {
+        ExecutionError executionError = new ExecutionError();
+        executionError.setFailTime(LocalDateTime.now());
+        executionError.setCalledMethod(invocationContext.getMethod());
+        executionError.setThrowable(throwable);
+        executionErrorEvent.fire(executionError);
     }
 }
---
+++ src/main/java/cz/pscheidl/benguard/event/ExecutionError.java
@@ -0,0 +1,37 @@
+package cz.pscheidl.benguard.event;
+
+import java.lang.reflect.Method;
+import java.time.LocalDateTime;
+
+/**
+ * @author Pavel Pscheidl
+ */
+public class ExecutionError {
+    private Method calledMethod;
+    private Throwable throwable;
+    private LocalDateTime failTime;
+
+    public Method getCalledMethod() {
+        return calledMethod;
+    }
+
+    public void setCalledMethod(Method calledMethod) {
+        this.calledMethod = calledMethod;
+    }
+
+    public Throwable getThrowable() {
+        return throwable;
+    }
+
+    public void setThrowable(Throwable throwable) {
+        this.throwable = throwable;
+    }
+
+    public LocalDateTime getFailTime() {
+        return failTime;
+    }
+
+    public void setFailTime(LocalDateTime failTime) {
+        this.failTime = failTime;
+    }
+}
